981abd717d676ffd14a92f8d966f4e7ec7ce80ae
Erdinc Atilgan <erdinc.atilgan@gmail.com>
2015-07-23 20:22:14 -0400

    admin features improved

--- app/controllers/manage_users_controller.rb
+++ app/controllers/manage_users_controller.rb
@@ -19,35 +19,35 @@ class ManageUsersController < ApplicationController
   	redirect_to users_url, notice: 'User was successfully deleted...'
   end
 
-# POST /items
-# POST /items.json
-def create
-  @user = User.create(user_params_for_new)
+  # POST /items
+  # POST /items.json
+  def create
+    @user = User.create(user_params_for_new)
 
-  respond_to do |format|
-    if @user.save
-      format.html { redirect_to users_url, notice: 'user was successfully created.' }
-      format.json { render :show, status: :created, location: users_url }
-    else
-      format.html { render :new }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_url, notice: 'user was successfully created.' }
+        format.json { render :show, status: :created, location: users_url }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
-# PATCH/PUT /items/1
-# PATCH/PUT /items/1.json
-def update
-  respond_to do |format|
-    if @user.update(user_params_for_edit)
-      format.html { redirect_to users_url, notice: 'User was successfully updated.' }
-      format.json { render :show, status: :ok, location: users_url }
-    else
-      format.html { render :edit }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+  # PATCH/PUT /items/1
+  # PATCH/PUT /items/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params_for_edit)
+        format.html { redirect_to users_url, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: users_url }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
 
 
